a97865c72bf320b1be86bb18e41c5a72f9a89528
bkupfer <bdokupferb@gmail.com>
2015-06-05 18:21:34 -0300

    adding login

--- crops_and_markets/urls.py
+++ crops_and_markets/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # index
-    url(r'^$', 'crops_and_markets_app.views.home', name='home'),
+    url(r'^$', 'crops_and_markets_app.views.login', name='login'),
     url(r'^index', 'crops_and_markets_app.views.home', name='home'),
     url(r'^home', 'crops_and_markets_app.views.home', name='home'),
     url(r'^about', 'crops_and_markets_app.views.about', name='about'),
--- crops_and_markets_app/views.py
+++ crops_and_markets_app/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render, render_to_response, RequestContext
 
-# Neutral pages
-def home(request):
-    return render_to_response("home.html", locals(), context_instance=RequestContext(request))
+from django.shortcuts import render, render_to_response, redirect, get_object_or_404
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
+from django.views.decorators.csrf import csrf_protect
+
+
+# Neutral pages
 def about(request):
     return render_to_response("about.html", locals(), context_instance=RequestContext(request))
 
+def home(request):
+    return render_to_response("home.html", locals(), context_instance=RequestContext(request))
+
+@csrf_protect
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            return redirect('home')
+
+        else:
+            print("error validating form -- form not valid")
+
+    return render_to_response("login.html", locals(), context_instance=RequestContext(request))
+
+def logout(request):
+    auth.logout(request)
+    return redirect(login)
 
 # Crops
 def crops(request):
