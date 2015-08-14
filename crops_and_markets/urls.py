e6163d1c6a881fcb4ea72c3efbfccda290bea310
Bernardo Jaime Küpfer Besa <bdokupferb@gmail.com>
2015-08-14 11:40:38 -0300

    adding potential table, and modifying market info

--- crops_and_markets/urls.py
+++ crops_and_markets/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
 	url(r'^markets', 'crops_and_markets_app.views.markets', name='markets'),
 	url(r'^market_map', 'crops_and_markets_app.views.market_map', name='market_map'),
 	url(r'^market_table', 'crops_and_markets_app.views.market_table', name='market_table'),
+	url(r'^market_table_potential', 'crops_and_markets_app.views.market_table_potential', name='market_table_potential'),
 	url(r'^add_market', 'crops_and_markets_app.views.add_market', name='add_market'),
 	url(r'^market_info', 'crops_and_markets_app.views.market_info', name='market_info')
 )
--- crops_and_markets_app/views.py
+++ crops_and_markets_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, render_to_response, RequestContext
 from django.shortcuts import render, render_to_response, redirect, get_object_or_404
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
-from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-
-# from crops_and_markets_app.models import *
+from django.views.decorators.csrf import csrf_protect
 from models import *
 from forms import *
 
 ############
 # Neutral pages
+@login_required
 def about(request):
 	return render_to_response("about.html", locals(), context_instance=RequestContext(request))
 
+@login_required
 def home(request):
 	return render_to_response("home.html", locals(), context_instance=RequestContext(request))
 
@@ -37,6 +37,7 @@ def login(request):
 def logout(request):
 	auth.logout(request)
 	return redirect(login)
+
 
 ############
 # Crops
@@ -71,22 +72,24 @@ def market_map(request):
 
 @login_required
 def market_table(request):
-	
 	clients = Client.objects.all()
-	print clients
 	return render_to_response("markets/market_table.html", locals(), context_instance=RequestContext(request))
+
+@login_required
+def market_table_potential(request):
+    clients = Client.objects.all()
+    return render_to_response("markets/market_table_potential.html", locals(), context_instance=RequestContext(request))
 
 @login_required
 def market_info(request):
 	if request.method == "GET" and 'id' in request.GET:
 		id = request.GET['id']
-	else:
-		id = -1
+        client = Client.objects.get(pk = id)
+
 	return render_to_response("markets/market_info.html", locals(), context_instance=RequestContext(request))
 
 @login_required
 def add_market(request):
-
 	client_form = ClientForm(request.POST or None)
 
 	if request.method == 'POST':
