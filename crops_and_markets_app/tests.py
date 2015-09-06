6ca6f3896a5002d0810a9b896f56c24ed32a8621
Bernardo Jaime Küpfer Besa <bdokupferb@gmail.com>
2015-09-06 18:19:59 -0300

    starting to add multiple geoinformations for a single client

--- crops_and_markets_app/tests.py
+++ crops_and_markets_app/tests.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.test import TestCase
 
 # Create your tests here.
 
+	# “If you wish to forget anything on the spot, make a note that this thing is to be remembered.”
+	#	― Edgar Allan Poe
