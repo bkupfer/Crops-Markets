cbdcbca2b10f7f6369470a80f9e3446339b39d9a
bkupfer <bdokupferb@gmail.com>
2015-09-22 09:49:53 -0300

    code styling

--- crops_and_markets_app/tests.py
+++ crops_and_markets_app/tests.py
@@ -4,4 +4,7 @@ from django.test import TestCase
 # Create your tests here.
 
 	# “If you wish to forget anything on the spot, make a note that this thing is to be remembered.”
-	#	― Edgar Allan Poe
+	#	―― Edgar Allan Poe
+
+	# “I cannot remember the books I've read any more than the meals I have eaten; even so, they have made me.” 
+	#	――-Ralph Ellison
